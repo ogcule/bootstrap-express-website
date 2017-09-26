d147b2214107545cce1868250a129fba9f00b0b7
Oliver Cule <ogcule@outlook.com>
2017-09-26 21:07:52 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 import 'bootstrap';
+import './app.scss';
+console.log("Hello from app.js")
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,71 @@
+const webpack = require('webpack');
 const path = require('path');
-
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname,'dist')
-  }
+  },
+  module: {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['env']
+        }
+      }
+    },
+    {
+    test: /\.(scss)$/,
+    use: [{
+      loader: 'style-loader',
+    }, {
+      loader: 'css-loader',
+    }, {
+      loader: 'postcss-loader',
+      options: {
+        plugins: function () {
+          return [
+            require('precss'),
+            require('autoprefixer')
+          ];
+        }
+      }
+    }, {
+      loader: 'sass-loader'
+    }
+  ]
+},
+/*{
+  test: /\.css$/,
+  use: ExtractTextPlugin.extract({
+  fallback: "style-loader",
+  use: "css-loader"
+  })
+}*/
+]
+},
+devServer: {
+  contentBase: path.join(__dirname, "dist"),
+  compress: true
+},
+plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+        Popper: ['popper.js', 'default']
+      }),
+    /*  new ExtractTextPlugin("dist/styles.css"),*/
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        filename: 'index.html'
+     })
+
+    ]
 }
