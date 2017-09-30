81c99d02824d67050c94ed87a3aad495d30e872d
ogcule <ogcule@outlook.com>
2017-09-30 18:53:21 +0100

    update

--- server/server.js
+++ server/server.js
@@ -14,11 +14,11 @@ var family = {
   'Daddy': 'The awesome one'
 };
 app.use(logger('combined'));
-app.use(express.static('./../dist'));
+app.use(express.static('.\..\dist\index.html'));
 app.get('/family', function(request, response){
-  response.json(family);
+   response.json(family);
 });
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
-  console.log('express server listening on port 8080');
+  console.log('express server listening on port 3000');
 });
