5800c22a94ebf3043fa7b1aef48f750253274441
ogcule <ogcule@outlook.com>
2017-10-13 07:25:09 +0100

    update

--- src/client.js
+++ src/client.js
@@ -52,17 +52,7 @@ $('.alert').on('click', '.btn-success', function () {
  })
 });
 
-
-  window.addEventListener("load", function() {
-    var form = document.getElementById("needs-validation");
-    form.addEventListener("submit", function(event) {
-      if (form.checkValidity() == false) {
-        event.preventDefault();
-        event.stopPropagation();
-      }
-      form.classList.add("was-validated");
-    }, false);
-  }, false);
+    
 
 
 });
